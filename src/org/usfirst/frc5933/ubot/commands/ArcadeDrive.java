2865af448ec7433c5f3e960a8bff84c4739bfa82
Grace Wise <thatgirl007.gw@gmail.com>
2016-01-27 22:07:08 -0700

    change names of joystick

--- src/org/usfirst/frc5933/ubot/commands/ArcadeDrive.java
+++ src/org/usfirst/frc5933/ubot/commands/ArcadeDrive.java
@@ -42,7 +42,7 @@ public class ArcadeDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.arcadeDrive(Robot.oi.getxbox());
+    	Robot.driveTrain.arcadeDrive(Robot.oi.getLeftJoystick());
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc5933/ubot/commands/TankDrive.java
+++ src/org/usfirst/frc5933/ubot/commands/TankDrive.java
@@ -42,7 +42,7 @@ public class TankDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.tankDrive(Robot.oi.getxbox(), Robot.oi.getlogitech());
+    	Robot.driveTrain.tankDrive(Robot.oi.getLeftJoystick(), Robot.oi.getRightJoystick());
     }
 
     // Make this return true when this Command no longer needs to run execute()
