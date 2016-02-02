dfbb28f4d8d226e29ec55bf65396cfd193e1cdff
Andrew Van Uitert <avanuitert@control4.com>
2016-02-01 20:05:38 -0700

    Added requires for arm subsystem

--- src/org/usfirst/frc5933/ubot/OI.java
+++ src/org/usfirst/frc5933/ubot/OI.java
@@ -64,9 +64,9 @@ public class OI {
 
         xBoxJoystick = new Joystick(0);
         
-        rightBumper = new JoystickButton(xBoxJoystick, 11);
+        rightBumper = new JoystickButton(xBoxJoystick, 5);
         rightBumper.whileHeld(new LiftingArmDown());
-        leftBumper = new JoystickButton(xBoxJoystick, 10);
+        leftBumper = new JoystickButton(xBoxJoystick, 4);
         leftBumper.whileHeld(new LiftingArmUp());
 
 
