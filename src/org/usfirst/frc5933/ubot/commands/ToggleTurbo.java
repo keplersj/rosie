13a9a8df0ce0774d747631fe002b39d5adb09deb
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-02-10 16:39:47 -0700

    Enable Turbo

--- src/org/usfirst/frc5933/ubot/commands/ToggleTurbo.java
+++ src/org/usfirst/frc5933/ubot/commands/ToggleTurbo.java
@@ -37,6 +37,7 @@ public class ToggleTurbo extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.driveTrain.toggleTurbo();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -45,7 +46,7 @@ public class ToggleTurbo extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
@@ -55,5 +56,6 @@ public class ToggleTurbo extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
