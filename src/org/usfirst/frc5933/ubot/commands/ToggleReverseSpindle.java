276784d3e4164715840c5c22f69cab6ffe55dba9
Andrew Van Uitert <avanuitert@control4.com>
2016-02-10 20:29:18 -0700

    Make the ball grabber subsystem work

--- src/org/usfirst/frc5933/ubot/commands/ToggleReverseSpindle.java
+++ src/org/usfirst/frc5933/ubot/commands/ToggleReverseSpindle.java
@@ -42,11 +42,12 @@ public class ToggleReverseSpindle extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.ballGrabberSubsystem.toggleReverse();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
@@ -56,5 +57,6 @@ public class ToggleReverseSpindle extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
