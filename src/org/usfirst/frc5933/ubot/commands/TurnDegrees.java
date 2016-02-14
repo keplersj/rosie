ae5fd680a591b095d27eb49ae9f9de07c5bb718d
Andrew Van Uitert <avanuitert@control4.com>
2016-02-13 21:26:24 -0700

    Turing the proper amount

--- src/org/usfirst/frc5933/ubot/commands/TurnDegrees.java
+++ src/org/usfirst/frc5933/ubot/commands/TurnDegrees.java
@@ -40,7 +40,7 @@ public class TurnDegrees extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.driveTrain.turnDegrees( 0.5 , 90 );
+        Robot.driveTrain.turnDegrees( 0.5 , -90 );
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -50,7 +50,7 @@ public class TurnDegrees extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
@@ -60,5 +60,6 @@ public class TurnDegrees extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
