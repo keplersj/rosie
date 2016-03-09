471220a5df59161fb873a107b40db3f961236124
Andrew Van Uitert <avanuitert@control4.com>
2016-03-09 09:21:44 -0700

    Trying to keep the 2 src code bases a little in sync

---
+++ src/org/usfirst/frc5933/ubot/PreferenceConstants.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc5933.ubot;
+
+
+public class PreferenceConstants {
+    public static final String BACKUP_VALUE                             = "unknown";
+    public static final String AUTONOMOUS_COMMAND_KEY                   = "AutonomousCommand";
+
+    public static final String AUTONOMOUS_COMMAND_POS1_LOWBAR           = "Pos1LowBar";
+    
+    public static final String AUTONOMOUS_COMMAND_POS2_MOTE             = "Pos2Mote";
+    public static final String AUTONOMOUS_COMMAND_POS2_ROCKWALL         = "Pos2RockWall";
+    public static final String AUTONOMOUS_COMMAND_POS2_RAMPART          = "Pos2Rampart";
+    public static final String AUTONOMOUS_COMMAND_POS2_ROUGHTERRAIN     = "Pos2RoughTerrain";
+
+    public static final String AUTONOMOUS_COMMAND_POS3_MOTE             = "Pos3Mote";
+    public static final String AUTONOMOUS_COMMAND_POS3_ROCKWALL         = "Pos3RockWall";
+    public static final String AUTONOMOUS_COMMAND_POS3_RAMPART          = "Pos3Rampart";
+    public static final String AUTONOMOUS_COMMAND_POS3_ROUGHTERRAIN     = "Pos3RoughTerrain";
+    
+    public static final String AUTONOMOUS_COMMAND_POS4_MOTE             = "Pos4Mote";
+    public static final String AUTONOMOUS_COMMAND_POS4_ROCKWALL         = "Pos4RockWall";
+    public static final String AUTONOMOUS_COMMAND_POS4_RAMPART          = "Pos4Rampart";
+    public static final String AUTONOMOUS_COMMAND_POS4_ROUGHTERRAIN     = "Pos4RoughTerrain";
+    
+    public static final String AUTONOMOUS_COMMAND_POS5_MOTE             = "Pos5Mote";
+    public static final String AUTONOMOUS_COMMAND_POS5_ROCKWALL         = "Pos5RockWall";
+    public static final String AUTONOMOUS_COMMAND_POS5_RAMPART          = "Pos5Rampart";
+    public static final String AUTONOMOUS_COMMAND_POS5_ROUGHTERRAIN     = "Pos5RoughTerrain";
+    
+    public static final String AUTONOMOUS_COMMAND_SPYBOX                = "SpyBox";
+    public static final String AUTONOMOUS_COMMAND_EC_DRIVESTRAIGHT      = "EncoderDriveStraight";
+    public static final String AUTONOMOUS_COMMAND_EC_TRURNDEGREES       = "EncoderTurnDegrees";
+    
+    public static final String DEBUG_SUBSYSTEM_DRIVE_TRAIN_KEY          = "DebugDriveTrain";
+}
