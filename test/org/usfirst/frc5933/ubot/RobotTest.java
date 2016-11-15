e3543d7cda42a283391932d4de21f6e4cdc804c7
Kepler Sticka-Jones <kepler+git@stickajones.org>
2016-11-15 15:41:14 -0700

    Working tests!
    With WPI mocking and code coverage!

---
+++ test/org/usfirst/frc5933/ubot/RobotTest.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc5933.ubot;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RobotTest {
+
+    static Robot robot;
+
+    @Before
+    public void beforeAll() {
+        robot = new Robot();
+    }
+
+    // Yes I know this is pointless, but I want that code coverage!
+    @Test
+    public void testModuleName() {
+        assertEquals("Rosie", robot.getModuleName());
+    }
+
+    @Test
+    public void testModuleVersion() {
+        assertEquals("1.0.0", robot.getModuleVersion());
+    }
+
+}
